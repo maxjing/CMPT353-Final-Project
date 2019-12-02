bf22949c26547b6b81914231437e232698d38e95
Jing Wen <tvandcc@gmail.com>
2019-12-01 22:01:57 -0800

    upload report and edit data_etl

--- data_etl.py
+++ data_etl.py
@@ -10,10 +10,6 @@ root_dir = os.path.abspath(os.path.dirname(sys.argv[0]) or '.')
 csv_folder = 'data_processed'
 files = glob('./data/*.csv')
 
-labels = ['walk', 'run', 'upstairs', 'downstairs']
-positions = ['foot', 'hand', 'pocket']
-observers = ['ax', 'ay', 'az', 'wx', 'wy', 'wz']
-
 path_re = re.compile(r"^(.+/)([^-]*)")
 type_re = re.compile(r"(.+)_")
 
@@ -27,6 +23,9 @@ def extractFileName(path):
 
 
 def extractTypeName(path):
+    '''
+    extract label
+    '''
     m = type_re.match(path)
     return m.group(1)
 
@@ -39,21 +38,35 @@ def extractData(df, min=5, max=15):
     return df.loc[condition].reset_index(drop=True)
 
 
+def noiseSmooth(df):
+    '''
+        use Butterworth filter
+    '''
+    b, a = signal.butter(3, 0.05, btype='lowpass', analog=False)
+    df['ax_filtered'] = signal.filtfilt(b, a, df['ax'])
+    df['ay_filtered'] = signal.filtfilt(b, a, df['ay'])
+    df['az_filtered'] = signal.filtfilt(b, a,  df['az'])
+    df['wx_filtered'] = signal.filtfilt(b, a, df['wx'])
+    df['wy_filtered'] = signal.filtfilt(b, a, df['wy'])
+    df['wz_filtered'] = signal.filtfilt(b, a,  df['wz'])
+    return df
+
+
 def main():
     os.mkdir(csv_folder)
+    dtypes1 = ['float', 'float', 'float', 'float', 'float', 'float',
+               'float', 'float', 'float', 'float', 'float', 'float', 'float', 'str']
+    dtypes2 = ['float', 'float', 'float', 'float', 'float', 'float', 'float']
     for f in files:
         fileName = extractFileName(f)
         typeName = extractTypeName(fileName)
-        original_df = pd.read_csv(f).dropna(axis='columns')
-        original_df = extractData(original_df)
-        original_df['label'] = typeName
-        original_df.to_csv(os.path.join(
-            f'{root_dir}/{csv_folder}/{fileName}.csv'), mode='a')
-        for observer in observers:
-            df = pd.DataFrame()
-            df[observer] = original_df[observer]
-            df.to_csv(os.path.join(
-                f'{root_dir}/{csv_folder}/{fileName}-{observer}.csv'), mode='a', header=False, index=False)
+        df = pd.read_csv(f).dropna(axis='columns')
+        df = extractData(df)
+        df = noiseSmooth(df)
+        df['label'] = typeName
+        df = df.drop(columns='time')
+        df.to_csv(os.path.join(
+            f'{root_dir}/{csv_folder}/{fileName}.csv'), mode='a', index=False, header=False)
 
 
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -1,36 +1,64 @@
 import sys
-from glob import glob
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from scipy import signal
-from scipy.signal import find_peaks_cwt
+import seaborn as sns
+import os
+cols = ['ax', 'ay', 'az', 'wx', 'wy', 'wz', 'ax_filtered', 'ay_filtered',
+        'az_filtered', 'wx_filtered', 'wy_filtered', 'wz_filtered', 'label']
+
+SENSOR_DATA_COLUMNS = ['ax_filtered', 'ay_filtered',
+                       'az_filtered', 'wx_filtered', 'wy_filtered', 'wz_filtered']
+
+walk_foot = pd.read_csv('data_processed/walk_foot.csv', names=cols)
+run_foot = pd.read_csv('data_processed/run_foot.csv', names=cols)
+upstairs_foot = pd.read_csv('data_processed/upstairs_foot.csv', names=cols)
+downstairs_foot = pd.read_csv('data_processed/downstairs_foot.csv', names=cols)
+
+walk_pocket = pd.read_csv('data_processed/walk_pocket.csv', names=cols)
+run_pocket = pd.read_csv('data_processed/run_pocket.csv', names=cols)
+upstairs_pocket = pd.read_csv('data_processed/upstairs_pocket.csv', names=cols)
+downstairs_pocket = pd.read_csv('data_processed/downstairs_pocket.csv', names=cols)
+
+walk_hand = pd.read_csv('data_processed/walk_hand.csv', names=cols)
+run_hand = pd.read_csv('data_processed/run_hand.csv', names=cols)
+upstairs_hand = pd.read_csv('data_processed/upstairs_hand.csv', names=cols)
+downstairs_hand = pd.read_csv('data_processed/downstairs_hand.csv', names=cols)
 
 
-def savePlot(df):
-    '''
-        visualization
-    '''
-    plt.figure(figsize=(15, 6))
-    plt.plot(df['time'], df['acceleration'], label='acceleration')
-    plt.plot(df, df['gyro'], label='gyro')
-    plt.xlabel('time')
-    plt.ylabel('a (m/s^2)')
-    plt.legend()
-    plt.title('a vs g')
-    plt.savefig()
-
-
-def getLocalMaximum(df):
-    indexes = find_peaks_cwt(df['g_removed'], np.arange(1, 550))
-    # plt.plot(df['a_remove_g'])
-    # plt.plot(indexes, df['a_remove_g'][indexes], "x")
-    # plt.plot(np.zeros_like(df['a_remove_g']), "--", color="gray")
-    # plt.show()
+def create_plots():
+    os.mkdir('fig')
+    for c in SENSOR_DATA_COLUMNS:
+        plt.figure(figsize=(10, 5))
+        plt.title('Foot')
+        sns.distplot(walk_foot[c], label='walk')
+        sns.distplot(run_foot[c], label='run')
+        sns.distplot(upstairs_foot[c], label='upstairs')
+        sns.distplot(downstairs_foot[c], label='downstairs')
+        plt.legend()
+        plt.savefig(f'./fig/foot-{c}.png')
+    for c in SENSOR_DATA_COLUMNS:
+        plt.figure(figsize=(10, 5))
+        plt.title('Hand')
+        sns.distplot(walk_hand[c], label='walk')
+        sns.distplot(run_hand[c], label='run')
+        sns.distplot(upstairs_hand[c], label='upstairs')
+        sns.distplot(downstairs_hand[c], label='downstairs')
+        plt.legend()
+        plt.savefig(f'./fig/hand-{c}.png')
+    for c in SENSOR_DATA_COLUMNS:
+        plt.figure(figsize=(10, 5))
+        plt.title('Pocket')
+        sns.distplot(walk_pocket[c], label='walk')
+        sns.distplot(run_pocket[c], label='run')
+        sns.distplot(upstairs_pocket[c], label='upstairs')
+        sns.distplot(downstairs_pocket[c], label='downstairs')
+        plt.legend()
+        plt.savefig(f'./fig/pocket-{c}.png')
 
 
 def main():
-    data = pd.read_csv(sys.argv[1]).dropna(axis='columns')
+    create_plots()
 
 
 if __name__ == '__main__':
